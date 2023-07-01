1b545ff70d207873337800f88082bf8f71861995
codernocook <90613458+codernocook@users.noreply.github.com>
2023-07-01 06:59:23 +0100

    Quest 1: Main menu
    in "index.js" file function "mainMenu_clicking" is for the main menu button. Try to make it work
    
    Hint: style

--- index.js
+++ index.js
@@ -1,5 +1,27 @@
 let ilikeblackmen = 0
-const clicking = function(){
-    ilikeblackmen++
-    document.getElementsByClassName("clickButton")[0].innerHTML = "click! | " + ilikeblackmen
+
+// This is the main menu trigger function
+const mainMenu_clicking = function() {
+    
 }
+
+// This is the gamePlay trigger function
+const gamePlay_clicking = function() {
+    ilikeblackmen++
+
+    if (document.getElementsByClassName("clickButton_gamePlay") && document.getElementsByClassName("clickButton")[0] && Number(ilikeblackmen)) {
+        document.getElementsByClassName("clickButton_gamePlay")[0].innerHTML = `click! | ${ilikeblackmen}`;
+    }
+}
+
+
+/* Database method, how to use?
+let database_lib = import("./lib");
+
+database_lib.then((func) => {
+    func.set(key, value),
+    func.delete(key),
+    func.has(key),
+    func.get(key)
+})
+*/
---
+++ lib.js
@@ -0,0 +1,22 @@
+const database_set = function() {
+
+}
+
+const database_delete = function() {
+
+}
+
+const database_get = function() {
+
+}
+
+const database_has = function() {
+
+}
+
+export {
+    database_set         as set,
+    database_delete      as delete,
+    database_get         as get,
+    database_has         as has,
+}
