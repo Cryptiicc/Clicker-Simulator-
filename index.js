911e19114699caeeed51c6cb03ba34bce5abafdc
codernocook <90613458+codernocook@users.noreply.github.com>
2023-07-01 17:21:36 +0100

    i beat ur challenge

--- index.js
+++ index.js
@@ -1,19 +1,35 @@
 let ilikeblackmen = 0
+let counter = 0;
 
 // This is the main menu trigger function
 const mainMenu_clicking = function() {
-    
+    document.getElementsByClassName("startMenu")[0].style.display = "none";
+    document.getElementsByClassName("gamePlay")[0].style.display = "block";
 }
 
 // This is the gamePlay trigger function
 const gamePlay_clicking = function() {
     ilikeblackmen++
+    counter++;
 
     if (document.getElementsByClassName("clickButton_gamePlay") && document.getElementsByClassName("clickButton")[0] && Number(ilikeblackmen)) {
         document.getElementsByClassName("clickButton_gamePlay")[0].innerHTML = `click! | ${ilikeblackmen}`;
     }
 }
 
+const anti_cheat = function() {
+    if (counter >= 25) {
+        alert("You're cheating!")
+    }
+
+    counter = 0;
+
+    setTimeout(() => {
+        anti_cheat()
+    }, 1000);
+}
+
+anti_cheat();
 
 /* Database method, how to use?
 let database_lib = import("./lib");
